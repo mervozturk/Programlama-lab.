51745cc3b417de37d46a224df8826a23009824ae
Merve Öztürk <47740036+mervozturk@users.noreply.github.com>
2019-03-17 15:37:28 +0300

    9. ders

---
+++ 09.py
@@ -0,0 +1,24 @@
+def kurus(a): #greedy yöntem
+    bozuk=[100,50,25,10,5,1]
+    sonuc=[]
+    sonuc2=[]
+    n=a
+    for i in range(6):
+        if(a%bozuk[i]==0):
+             while(a>0):
+                 a=a-bozuk[i]
+                 sonuc.append(bozuk[i])
+
+    while(n!=0):
+        for i in range(0,6):
+            while(n>=bozuk[i]):
+                n=n-bozuk[i]
+                sonuc2.append(bozuk[i])
+
+    if(len(sonuc)<len(sonuc2)):
+        return sonuc
+    else:
+        return sonuc2
+
+x=int(input("bir sayı giriniz:"))
+print("sonuc:",kurus(x))
